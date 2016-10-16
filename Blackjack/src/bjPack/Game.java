c9361f5f2df3f20c57b7df26cb7becc1c6cd8a11
HulkLogan <lrcsms1985@gmail.com>
2016-10-16 15:43:24 -0400

    final push for this branch

--- Blackjack/src/bjPack/Game.java
+++ Blackjack/src/bjPack/Game.java
@@ -5,59 +5,125 @@ import java.util.List;
 
 public class Game {
 	public static void main(String args[]) { 
-		int k = 1;
-		Deck deck1 = new Deck();
-		//ints for hands
-		
-		
-		//booleans for hit/stay
-		
-		
-		
-		
-//	//tests deck creation
-//		for (Card c :deck1.deckList) {
-//			System.out.println(k);
-//			System.out.println(c.toString());
-//			k++;
-//		}
-//		
-//		int c= 1;
-//		for(int i = 0; i<52; i++) {
-//			//tests deal
-//			Card newCard;
-//			newCard = deck1.deal(); 
-//			System.out.print("card " + c + " ");
-//			System.out.println(newCard.toString());
-//	
-//			c++;
-//		}
 	
 		//performs initial deal using fresh deck
 		Deck deck2 = new Deck();
 		List<Card> playerOneHand = new ArrayList<Card>();
 		List<Card> dealerHand = new ArrayList<Card>();
 		playerOneHand.add(deck2.deal());
-		dealerHand.add(deck2.deal());
 		playerOneHand.add(deck2.deal());
+		dealerHand.add(deck2.deal());
 		dealerHand.add(deck2.deal());
 		System.out.print("player one's hand is: ");
 		printList(playerOneHand);
+		printList(dealerHand);
 		System.out.println("");
 		System.out.print("dealer's hand is: ");
-		printList(dealerHand);
-		
-		//while loop - needs to run until 21, then check for ACE, reassign hand value if found
-		//end match if not. match also ends if both players pick stay. 
 	}
 	
+	/************************************************************************
+	 * dealerHit() is the rudimentary A.I. for the dealer. Right now, dealer
+	 * only has one option, to hit if hand count is less than 17. In all 
+	 * other cases dealer stays.
+	 * @param dealerHand dealers hand
+	 * @return boolean true if dealer can hit
+	 ************************************************************************/
+	public static boolean dealerHit(List <Card> dealerHand){
+		int count = handCount(dealerHand);
+		if(count < 17){
+			return true;
+		}
+		else
+			return false;
+		
+	}
+	
+	/************************************************************************
+	 * hasAce() checks a hand for an Ace value and returns true if found
+	 * @param hand the hand to be checked for an Ace
+	 * @return boolean true if Ace Card() is found in hand
+	 ************************************************************************/
+	public static boolean hasAce(List<Card> hand){
+		int bool = 0;
+			for(Card card :hand){
+				switch(card.rank()){
+				case ACE:
+					bool = 1;
+					break;
+				}
+			}
+			if(bool == 1){
+				return true;
+				}
+			else 
+				return false;
+	}
+	
+	/************************************************************************
+	 * printList() prints out a particular hand
+	 * @param dealerHand
+	 ************************************************************************/
 	static void printList(List<Card> dealerHand){
 		for (Card card :dealerHand) {
 	 		System.out.print(card.toString());
 	 		System.out.print("  ");
-	 	}
+	 		}
+		
 	 }
 	
-	
-//Static hand value - assign number value to hand based on enum
+   /*************************************************************************
+	*handCount() takes a hand, iterates through the cards in the hand, and 
+	*counts the values of the cards, adding them and returning the hand count
+	* @param hand the hand you wish to count
+	* @return handCount int representing the hands value
+	*************************************************************************/
+	static int handCount(List<Card> hand){
+		int count = 0;
+		for(Card card :hand){
+			switch(card.rank()){
+			case TWO:
+				count += 2;
+				break;
+			case THREE:
+				count += 3;
+				break;
+			case FOUR:
+				count += 4;
+				break;
+			case FIVE:
+				count += 5;
+				break;
+			case SIX:
+				count += 6;
+				break;
+			case SEVEN:
+				count += 7;
+				break;
+			case EIGHT:
+				count += 8;
+				break;
+			case NINE:
+				count += 9;
+				break;
+			case TEN:
+				count += 10;
+				break;
+			case JACK:
+				count += 10;
+				break;
+			case QUEEN:
+				count += 10;
+				break;
+			case KING:
+				count += 10;
+				break;
+			case ACE:
+				count += 11;
+				break;
+			}
+		}
+		if(hasAce(hand)==true && count > 21)
+			count -= 10;
+		return count;
+	}
 }
