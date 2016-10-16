9b6d345902aeaaf6064991520e02086afba68601
HulkLogan <lrcsms1985@gmail.com>
2016-10-16 14:30:47 -0400

    Full commenting for Card and Deck

--- Blackjack/src/bjPack/Deck.java
+++ Blackjack/src/bjPack/Deck.java
@@ -1,11 +1,19 @@
+/***************************************************************************
+* Deck() - used to create a 'deck' of Card objects using an array. 
+* @author Logan Crowe, Edward Johnson, Anthony Dowling
+***************************************************************************/
 package bjPack;
 import java.util.*;
-
 import bjPack.Card.Rank;
 import bjPack.Card.Suit;
 public class Deck {
-List<Card> deckList = new ArrayList<Card>();
 	
+	/*the Arraylist that holds the deck*/	
+	List<Card> deckList = new ArrayList<Card>();
+	
+	/*********************************************************************
+	 * Deck() creates an array of Card objects and shuffles them. 
+	 *********************************************************************/
 	public Deck (){
     	for(Suit suit: Suit.values()) {
     		for(Rank value: Rank.values()) {
@@ -14,17 +22,34 @@ List<Card> deckList = new ArrayList<Card>();
     	}
  		Collections.shuffle(deckList);
 	}
-
-/***********************************************************************
-* Returns card object, "moves" through deck using subList
-**********************************************************************/	
+	
+	/*********************************************************************
+	 * deal() is used to deal a single card from the deck. The decks
+	 * size is revalued to take into account the missing card. 
+	 * @return Card a card from the deck. 
+	 *********************************************************************/
 	public Card deal(){
-	int deckSize = deckList.size();
-	Card dealtCard;
-	dealtCard = deckList.get(0);
-	deckList = deckList.subList(1, deckSize);
-	return dealtCard;
-}
+		int deckSize = deckList.size();
+		Card dealtCard;
+		dealtCard = deckList.get(0);
+		deckList = deckList.subList(1, deckSize);
+		return dealtCard;
+	}
+	
+	/*********************************************************************
+	 * @return deckCount an int that represents the current size of the 
+	 * deck.
+	 *********************************************************************/
+	public int deckCount(){
+		return deckList.size();
+	}
+	
+	/*********************************************************************
+	 * Shuffles the deck. 
+	 *********************************************************************/
+	public void shuffleDeck(){
+		Collections.shuffle(deckList);
+	}
 
 
 }
